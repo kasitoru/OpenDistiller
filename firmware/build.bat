a9226871afd356fc5b30224e33002a09e244187a
avdeevsv91 <avdeevsv91@gmail.com>
2022-12-22 11:49:51 +0300

    Update build.bat

--- firmware/build.bat
+++ firmware/build.bat
@@ -5,27 +5,40 @@
 @ECHO off
 CHCP 65001 > nul
 
+REM ==================================================
 SET "VERSION=1.0"
 SET "LANGUAGE=RU"
+SET "MODULES=UART,BLUETOOTH"
+
 SET "CROSS_COMPILE=C:\Program Files\avr-gcc\bin\avr-"
-SET "CCFLAGS=-Wall -Os -mmcu=atmega328p -DF_CPU=16000000L -ffunction-sections -fdata-sections -Wl,--gc-sections,-u,vfprintf -lprintf_flt --param=min-pagesize=0"
+REM ==================================================
 
 DEL /F /Q firmware.hex
 RMDIR /S /Q temp
 MKDIR temp
 CD temp
 
-ECHO Компиляция libraries/u8g2...
-CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DU8X8_USE_PINS -DMUI_MAX_TEXT_LEN=64 -c ../libraries/u8g2/csrc/*.c"
+SET "DEFINES="
+SETLOCAL EnableDelayedExpansion
+FOR %%M IN ("%MODULES:,=" "%") DO (
+    IF NOT %%M == "" (
+        IF NOT %%M == ",=" (
+            SET "DEFINES=!DEFINES! -D%%~M"
+            SET "MODULE_%%~M=1"
+        )
+    )
+)
 
-ECHO Компиляция libraries/u8x8_avr...
-CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DU8X8_USE_PINS -I ../libraries/u8g2/csrc/ -c ../libraries/u8x8_avr/*.c"
+SET "CCFLAGS=-Wall -Os -mmcu=atmega328p -DF_CPU=16000000L -ffunction-sections -fdata-sections -Wl,--gc-sections,-u,vfprintf -lprintf_flt --param=min-pagesize=0"
 
 ECHO Компиляция libraries/avr-ds18b20...
 CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DONEWIRE_AUTO_CLI -I ../libraries/avr-ds18b20/include/ -c ../libraries/avr-ds18b20/src/*.c"
 
-ECHO Компиляция libraries/uart...
-CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/uart/*.c"
+ECHO Компиляция libraries/u8x8_avr...
+CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DU8X8_USE_PINS -I ../libraries/u8g2/csrc/ -c ../libraries/u8x8_avr/*.c"
+
+ECHO Компиляция libraries/u8g2...
+CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DU8X8_USE_PINS -DMUI_MAX_TEXT_LEN=64 -c ../libraries/u8g2/csrc/*.c"
 
 ECHO Компиляция libraries/tone...
 CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/tone/*.c"
@@ -33,11 +46,22 @@ CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/tone/*.c"
 ECHO Компиляция libraries/functions.c...
 CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/functions.c"
 
-ECHO Компиляция libraries/debug...
-CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/debug/*.c"
+IF DEFINED MODULE_DEBUG (
+    ECHO Компиляция libraries/debug...
+    CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/debug/*.c"
+    IF NOT DEFINED MODULE_UART (
+        SET "MODULE_UART=1"
+        SET "DEFINES=%DEFINES% -DUART"
+    )
+)
+
+IF DEFINED MODULE_UART (
+    ECHO Компиляция libraries/uart...
+    CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -c ../libraries/uart/*.c"
+)
 
 ECHO Компиляция main.c...
-CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DVERSION=\"%VERSION%\" -DUART -DBLUETOOTH -DLANG=%LANGUAGE% -DU8X8_USE_PINS -DMUI_MAX_TEXT_LEN=64 -I ../libraries/u8g2/csrc/ -c ../main.c"
+CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% -DVERSION=\"%VERSION%\" %DEFINES% -DLANG=%LANGUAGE% -DU8X8_USE_PINS -DMUI_MAX_TEXT_LEN=64 -I ../libraries/u8g2/csrc/ -c ../main.c"
 
 ECHO Линковка файлов...
 CMD /C ""%CROSS_COMPILE%gcc.exe" %CCFLAGS% *.o -o firmware.o"
